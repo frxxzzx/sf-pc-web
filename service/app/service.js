659c0f7a1b2fdb7d8b53dc56bb73e4682c6330d8
wx <1947147481@qq.com>
2019-06-26 10:29:06 +0800

    Initial commit

--- service/app/service.js
+++
@@ -1,5 +0,0 @@
-var Service = require("sf-inner-service");
-var file_os = require("fs");
-let config = JSON.parse(file_os.readFileSync("config.json", "utf-8"));
-console.log("sf-pc-web");
-Service.start(config);
