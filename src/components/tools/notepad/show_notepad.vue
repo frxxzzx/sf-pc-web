27cec12a9088afea96ca28a813648eadce2d2014
Xiao dun <1947147481@qq.com>
2019-06-28 12:10:53 +0800

    恢复被误删除的功能

--- src/components/tools/notepad/show_notepad.vue
+++ src/components/tools/notepad/show_notepad.vue
@@ -5,7 +5,10 @@ export default {
   name: "show_notepad",
   render(createElement) {
     let imgStuffixList = [".jpg", ".jpeg", ".gif", ".png"];
-    let pattern = RegExp(`(http|https|${BASE64_IMG_PROTOCOL}):\/\/[\\S]+`, "g");
+    let pattern = RegExp(
+      `(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;\u4e00-\u9fa5]+[-A-Za-z0-9+&@#/%=~_|\u4e00-\u9fa5]`,
+      "g"
+    );
     let lastIndex = 0;
     let renderList = [];
     let result;
@@ -68,6 +71,10 @@ export default {
         lastIndex = pattern.lastIndex;
       }
     }
+    //处理后面的元素
+    renderList.push(
+      this.data.content.substring(lastIndex, this.data.content.length)
+    );
     return createElement(
       "div",
       {
